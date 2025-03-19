ce0c0aac04bcae73f9fa0a71cb8403a612fa9dff
JFK20 <jfk,gangelt@gmail.com>
2025-03-19 12:09:32 +0100

    migration to NvChad 2.5

---
+++ mappings.lua
@@ -0,0 +1,29 @@
+require "nvchad.mappings"
+
+-- add yours here
+
+local map = vim.keymap.set
+
+map("n", "<leader>db", "<cmd>DapToggleBreakpoint<CR>", { desc = "Debug Add breakpoint to current line" })
+map("n", "<leader>dc", function ()
+  require("dap").continue()
+end, {desc = "Continue debugging"})
+map("n", "<leader>dus", function()
+    local widgets = require("dap.ui.widgets")
+    local sidebar = widgets.sidebar(widgets.scopes)
+    sidebar.open()
+  end, { desc = "Debug: Open debugging sidebar" })
+  
+-- DAP-Go mappings
+map("n", "<leader>dgt", function()
+    require("dap-go").debug_test()
+end, { desc = "Debug: Run Go test" })
+  
+map("n", "<leader>dgl", function()
+    require("dap-go").debug_last()
+end, { desc = "Debug: Run last Go test" })
+
+map("n", ";", ":", { desc = "CMD enter command mode" })
+map("i", "jk", "<ESC>")
+
+-- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
---
+++ plugins/init.lua
@@ -0,0 +1,48 @@
+return {
+  {
+    "stevearc/conform.nvim",
+    -- event = 'BufWritePre', -- uncomment for format on save
+    opts = require "configs.conform",
+  },
+
+  -- These are some examples, uncomment them if you want to see them work!
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require "configs.lspconfig"
+    end,
+  },
+  {
+  	"nvim-treesitter/nvim-treesitter",
+  	opts = {
+  		ensure_installed = {
+  			"vim", "lua", "vimdoc",
+        "html", "css"
+  	  },
+    },
+  },
+  {
+    'MeanderingProgrammer/render-markdown.nvim',
+    dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" },
+    lazy=false,
+    config = function()
+        require('render-markdown').setup({})
+        require('render-markdown').enable()
+    end,
+  },
+  {
+    "mfussenegger/nvim-dap",
+    init = function()
+      -- require("core.utils").load_mappings("dap")
+    end
+  },
+  {
+    "leoluz/nvim-dap-go",
+    ft = "go",
+    dependencies = "mfussenegger/nvim-dap",
+    config = function(_, opts)
+      require("dap-go").setup(opts)
+      -- require("core.utils").load_mappings("dap_go")
+    end
+  },
+}
